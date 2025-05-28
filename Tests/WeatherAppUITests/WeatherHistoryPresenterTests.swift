59722a20fb29aa3dc5a97c2cf066269ddd8edd15
VaishnaviMindstix <vaishnavi.deshmukh@mindstix.com>
2025-05-28 14:41:27 +0530

    WeatherHistoryPresenterTests added

--- Tests/WeatherAppUITests/WeatherHistoryPresenterTests.swift
+++ Tests/WeatherAppUITests/WeatherHistoryPresenterTests.swift
@@ -6,3 +6,94 @@
 //
 
 import XCTest
+@testable import WeatherAppUI
+@testable import WeatherDataSharedModel
+
+@available(iOS 13.0, *)
+final class WeatherHistoryPresenterTests: XCTestCase {
+    
+    final class MockWeatherHistoryInteractor: WeatherHistoryInteractorProtocol {
+        var weatherItems: [WeatherDataSharedModel] = []
+        var addItemCalled = false
+        var deleteItemCalled = false
+        
+        func fetchWeatherItems() -> [WeatherDataSharedModel] {
+            return weatherItems
+        }
+        
+        func addWeatherItem(_ item: WeatherDataSharedModel) {
+            addItemCalled = true
+            weatherItems.append(item)
+        }
+        
+        func deleteItem(at offsets: IndexSet) {
+            deleteItemCalled = true
+            weatherItems.remove(atOffsets: offsets)
+        }
+    }
+    
+    var presenter: WeatherHistoryPresenter!
+    var mockInteractor: MockWeatherHistoryInteractor!
+    
+    override func setUp() {
+        super.setUp()
+        mockInteractor = MockWeatherHistoryInteractor()
+        presenter = WeatherHistoryPresenter(interactor: mockInteractor)
+    }
+    
+    override func tearDown() {
+        presenter = nil
+        mockInteractor = nil
+        super.tearDown()
+    }
+    
+    func testLoadItems() {
+        let mockItem = sampleItem()
+        mockInteractor.weatherItems = [mockItem]
+        presenter.loadItems()
+        XCTAssertEqual(presenter.items.count, 1)
+        XCTAssertEqual(presenter.items.first?.city, "Test City")
+    }
+    
+    func testAddItem() {
+        let item = sampleItem()
+        presenter.addItem(data: item)
+        XCTAssertTrue(mockInteractor.addItemCalled)
+        XCTAssertEqual(presenter.items.count, 1)
+        XCTAssertEqual(presenter.items.first?.city, "Test City")
+    }
+    
+    func testAddSampleItem() {
+        presenter.addSampleItem()
+        XCTAssertTrue(mockInteractor.addItemCalled)
+        XCTAssertEqual(presenter.items.count, 1)
+        XCTAssertEqual(presenter.items.first?.city, "--")
+    }
+    
+    func testDeleteItem() {
+        let item1 = sampleItem(city: "City 1")
+        let item2 = sampleItem(city: "City 2")
+        mockInteractor.weatherItems = [item1, item2]
+        presenter.loadItems()
+        presenter.deleteItem(at: IndexSet(integer: 0))
+        XCTAssertTrue(mockInteractor.deleteItemCalled)
+        XCTAssertEqual(presenter.items.count, 1)
+        XCTAssertEqual(presenter.items.first?.city, "City 2")
+    }
+
+    private func sampleItem(city: String = "Test City") -> WeatherDataSharedModel {
+        WeatherDataSharedModel(
+            city: city,
+            date: "May 27, 2025",
+            isNight: false,
+            day: "Tuesday",
+            currentTemp: "25",
+            condition: "Clear",
+            conditionId: 800,
+            symbolName: "sun.max",
+            forecastDay: nil,
+            forecastNight: nil
+        )
+    }
+}
+

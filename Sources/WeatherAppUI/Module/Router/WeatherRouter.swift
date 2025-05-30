021ffc92974e6dc54a6740e6e5dccabe390120da
VaishnaviMindstix <vaishnavi.deshmukh@mindstix.com>
2025-05-30 16:15:10 +0530

    Update WeatherRouter.swift

--- Sources/WeatherAppUI/Module/Router/WeatherRouter.swift
+++ Sources/WeatherAppUI/Module/Router/WeatherRouter.swift
@@ -10,16 +10,16 @@ import CoreData
 
 @available(iOS 14.0, *)
 
-public final class WeatherRouter {
-    public static func createModule(city: City, apiKey: String) -> WeatherLandingView {
+final class WeatherRouter {
+    static func createModule(city: CityModel, apiKey: String) -> WeatherLandingView {
         let context = PersistenceController.shared.container.viewContext
         let presenter = WeatherPresenter()
         let interactor = WeatherInteractor()
         let interactorHistory = WeatherHistoryInteractor(context: context)
-        let presenterHistory = WeatherHistoryPresenter(interactor: interactorHistory)
+//        let presenterHistory = WeatherHistoryPresenter(interactor: interactorHistory)
         presenter.interactor = interactor
         interactor.presenter = presenter
-        interactor.presenterHistory = presenterHistory
+        interactor.interactorHistory = interactorHistory
         interactor.city = city
         interactor.apiKey = apiKey
         return WeatherLandingView(presenter: presenter)
